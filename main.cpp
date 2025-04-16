da382658976bf66bfb8d4fb515315af983eb4aca
Nicholas Mountain <nmountain@unc.edu>
2025-04-16 18:45:02 -0400

    api route to send actuator command

--- main.cpp
+++ main.cpp
@@ -23,10 +23,20 @@ int main()
         };
          res.set_content(response.dump(), "application/json"); });
 
-    svr.Get(R"(/sensor/(\d+))", [&](const Request &req, httplib::Response &res)
+    svr.Get(R"(/sensor/(\d+))", [&](const Request &req, Response &res)
             {
         int sensorId = std::stoi(req.matches[1]);
         res.set_content(manager.getSensorData(sensorId), "text/plain"); });
+
+    svr.Post(R"(/actuator/(\d+))", [&](const Request &req, Response &res)
+             {
+        int actuatorId = std::stoi(req.matches[1]);
+        std::string message = req.body;
+        manager.sendActuatorCommand(actuatorId, message);
+        json response = {
+            {"message", message}
+        };
+        res.set_content(response.dump(), "application/json"); });
 
     svr.listen("0.0.0.0", 8080);
 }
